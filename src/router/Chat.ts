cbfafd4152310f05ee0614b1f1dec2e2efb3453d
soyoka <55011525+soy0ka@users.noreply.github.com>
2024-07-01 19:02:39 +0900

    feat: chatreport

--- src/router/Chat.ts
+++ src/router/Chat.ts
@@ -60,4 +60,19 @@ app.post('/room/:id', async (req: Request, res: Response) => {
   return res.status(200).send(Formatter.format(true, 'OK')).end()
 })
 
+app.post('/report', async (req: Request, res: Response) => {
+  const { id, reason } = req.body
+  if (!id || !reason) return res.status(400).send(Formatter.format(false, 'Bad Request')).end()
+  const message = await prisma.message.findUnique({ where: { id: Number(id) } })
+  console.log(message)
+  await prisma.chatReportLog.create({
+    data: {
+      chatRoomId: message?.chatRoomId ?? 0,
+      content: message?.content || '',
+      reason,
+      userId: res.locals.user.id
+    }
+  })
+})
+
 export default app
