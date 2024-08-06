8d267bb0257a29d86e64ed73ac2561f812928531
soyoka <55011525+soy0ka@users.noreply.github.com>
2024-08-07 01:28:15 +0900

    feat: remove unused next parameter from error handler

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,9 +1,9 @@
 import { ApiStatusCode, CustomErrorCode } from '@/types/response'
 import ResponseFormatter from '@/utils/formatter/response'
 import { Logger } from '@/utils/logging/logger'
-import { NextFunction, Request, Response } from 'express'
+import { Request, Response } from 'express'
 
-const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+const errorHandler = (err: any, req: Request, res: Response) => {
   Logger.error(err.name).put(err.stack).out()
   res.status(ApiStatusCode.INTERNAL_SERVER_ERROR).send(ResponseFormatter.error(CustomErrorCode.UNKNOWN_ERROR)).end()
 }
