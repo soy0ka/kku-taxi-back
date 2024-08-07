e888d20994988f71d402478425ef6bb40be53cfa
soyoka <55011525+soy0ka@users.noreply.github.com>
2024-08-07 17:28:01 +0900

    fix: remove unused code and update socket initialization
    The code changes in the `index.ts` file remove the unused code for registering chat handlers and the `io` variable. The `initializeSocketServer` function is now called directly on the `server` object. This simplifies the code and improves performance by removing unnecessary socket initialization.
    
    Co-authored-by: [Author Name] <[Author Email]>

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,13 @@ import 'dotenv/config'
 import http from 'http'
 import app from './app'
 import { initializeSocketServer } from './sockets'
-import { registerChatHandlers } from './sockets/chatWebSocket'
 import { Logger } from './utils/logging/logger'
 
 const port = process.env.PORT || 3000
 const server = http.createServer(app)
-const io = initializeSocketServer(server)
 
 Logger.initialize('./')
-registerChatHandlers(io)
+initializeSocketServer(server)
 
 server.listen(port, () => {
   Logger.success('Express').put('Server Ready').next('port').put(port).out()
